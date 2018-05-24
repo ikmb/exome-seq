f5d33b2d93f107526c85d3d353021ce27c27e0ca
marchoeppner <mphoeppner@gmail.com>
2018-05-24 14:54:06 +0200

    Added md5 check option for sample sheet script

--- bin/samplesheet_from_folder.rb
+++ bin/samplesheet_from_folder.rb
@@ -12,6 +12,7 @@ opts.banner = "Reads Fastq files from a folder and writes a sample sheet to STDO
 opts.separator ""
 opts.on("-f","--folder", "=FOLDER","Folder to scan") {|argument| options.folder = argument }
 opts.on("-c","--centre", "=CENTRE","Name of sequencing centre") {|argument| options.centre = argument }
+opts.on("-s","--sanity", "Perform sanity check of md5 sums") { options.sanity = true }
 opts.on("-h","--help","Display the usage information") {
  puts opts
  exit
@@ -28,6 +29,9 @@ fastq_files = Dir["#{options.folder}/*_R*.fastq.gz"]
 
 groups = fastq_files.group_by{|f| f.split("/")[-1].split(/_L0/)[0] }
 
+warn "Building input sample sheet from FASTQ folder"
+warn "Performing sanity check on md5sums" if options.sanity
+
 puts "IndivID;SampleID;libraryID;rgID;rgPU;platform;platform_model;Center;Date;R1;R2"
 
 #G00076-L2_S19_L003_R1_001.fastq.gz
@@ -35,11 +39,24 @@ puts "IndivID;SampleID;libraryID;rgID;rgPU;platform;platform_model;Center;Date;R
 # group = the library id, may be split across lanes
 groups.each do |group, files|
 
+	warn "...processing library #{group}"
+
 	pairs = files.group_by{|f| f.split("/")[-1].split(/_R[1,2]/)[0] }
 
 	pairs.each do |p,reads|
 
         	left,right = reads.sort.collect{|f| File.absolute_path(f)}
+
+		if options.sanity
+			Dir.chdir(options.folder) {
+				[left,right].each do |fastq|
+					fastq_simple = fastq.split("/")[-1].strip
+					raise "Aborting - no md5sum found for fastq file #{fastq}" unless File.exists?(fastq_simple + ".md5")
+					status = `md5sum -c #{fastq_simple}.md5`
+					raise "Aborting - failed md5sum check for #{fastq}" unless status.strip.include?("OK")
+				end
+			}
+		end
 
         	library = group.split("_")[0]
         	sample = group.split("-")[0]
